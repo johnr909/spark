bbd6e90ef4f58314f7832da37705e4371fed0b52
john <johnjrose@gmail.com>
2020-05-26 15:20:28 -0600

    commented out unneeded  sidebar registrations

--- functions.php
+++ functions.php
@@ -117,42 +117,42 @@ add_action( 'after_setup_theme', 'wp_bootstrap_starter_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function wp_bootstrap_starter_widgets_init() {
-    register_sidebar( array(
-        'name'          => esc_html__( 'Sidebar', 'wp-bootstrap-starter' ),
-        'id'            => 'sidebar-1',
-        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3 class="widget-title">',
-        'after_title'   => '</h3>',
-    ) );
-    register_sidebar( array(
-        'name'          => esc_html__( 'Footer 1', 'wp-bootstrap-starter' ),
-        'id'            => 'footer-1',
-        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3 class="widget-title">',
-        'after_title'   => '</h3>',
-    ) );
-    register_sidebar( array(
-        'name'          => esc_html__( 'Footer 2', 'wp-bootstrap-starter' ),
-        'id'            => 'footer-2',
-        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3 class="widget-title">',
-        'after_title'   => '</h3>',
-    ) );
-    register_sidebar( array(
-        'name'          => esc_html__( 'Footer 3', 'wp-bootstrap-starter' ),
-        'id'            => 'footer-3',
-        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3 class="widget-title">',
-        'after_title'   => '</h3>',
-    ) );
+    // register_sidebar( array(
+    //     'name'          => esc_html__( 'Sidebar', 'wp-bootstrap-starter' ),
+    //     'id'            => 'sidebar-1',
+    //     'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+    //     'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    //     'after_widget'  => '</section>',
+    //     'before_title'  => '<h3 class="widget-title">',
+    //     'after_title'   => '</h3>',
+    // ) );
+    // register_sidebar( array(
+    //     'name'          => esc_html__( 'Footer 1', 'wp-bootstrap-starter' ),
+    //     'id'            => 'footer-1',
+    //     'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+    //     'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    //     'after_widget'  => '</section>',
+    //     'before_title'  => '<h3 class="widget-title">',
+    //     'after_title'   => '</h3>',
+    // ) );
+    // register_sidebar( array(
+    //     'name'          => esc_html__( 'Footer 2', 'wp-bootstrap-starter' ),
+    //     'id'            => 'footer-2',
+    //     'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+    //     'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    //     'after_widget'  => '</section>',
+    //     'before_title'  => '<h3 class="widget-title">',
+    //     'after_title'   => '</h3>',
+    // ) );
+    // register_sidebar( array(
+    //     'name'          => esc_html__( 'Footer 3', 'wp-bootstrap-starter' ),
+    //     'id'            => 'footer-3',
+    //     'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+    //     'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    //     'after_widget'  => '</section>',
+    //     'before_title'  => '<h3 class="widget-title">',
+    //     'after_title'   => '</h3>',
+    // ) );
 
 	/*--------------------------------------------------------------
 ## Spark  Widgets

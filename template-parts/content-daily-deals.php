2dd8063b9be0083b75ba49ced688fad5fda301d8
john <johnjrose@gmail.com>
2020-06-03 15:38:26 -0600

    first round of daily deal edits per Kim

--- template-parts/content-daily-deals.php
+++ template-parts/content-daily-deals.php
@@ -19,46 +19,41 @@
 
     <div class="row deals">
 
-        <div id="day-Sunday" class="col-md card">
+        <div id="day-Sunday" class="col card">
 		    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-sunday") ) : ?>
 		    <?php endif;?>
         </div>
 
-        <div id="day-Monday" class="col-md card">
+        <div id="day-Monday" class="col card">
 		    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-monday") ) : ?>
 		    <?php endif;?>
         </div>
 
-        <div id="day-Tuesday" class="col-md card">
+        <div id="day-Tuesday" class="col card">
 		    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-tuesday") ) : ?>
 		    <?php endif;?>
         </div>
 
-        <div id="day-Wednesday" class="col-md card">
+        <div id="day-Wednesday" class="col card">
 		    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-wednesday") ) : ?>
 		    <?php endif;?>
         </div>
 
-    </div><!-- .row -->
-
-    <div class="row deals">
-
-        <div id="day-Thursday" class="col-md card">
+        <div id="day-Thursday" class="col card">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-thursday") ) : ?>
 			<?php endif;?>
         </div>
 
-        <div id="day-Friday" class="col-md card">
+        <div id="day-Friday" class="col card">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-friday") ) : ?>
 			<?php endif;?>
         </div>
 
-        <div id="day-Saturday" class="col-md card">
+        <div id="day-Saturday" class="col card">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("deal-saturday") ) : ?>
 			<?php endif;?>
         </div>
 
-        <div class="col-sm" style="margin: 5px; padding: 5px;"></div>
 
     </div><!-- .row -->
 

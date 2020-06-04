78a2d8a75f66c3fd887fd946acd8d0a654e801c5
john <johnjrose@gmail.com>
2020-06-04 15:19:52 -0600

    just adding in a file

---
+++ template-parts/content-order-online-cta.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template part for order online CTA
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col">
+            <h3 class="lead">
+            	</i>&nbsp;&nbsp;Order Online: Its' as easy as 1-2-3!
+            </h3>
+        </div>
+    </div><!-- .row -->
+
+    <div class="row">
+
+       <div class="col-md coupon rounded online-cta">
+		    <div>
+		    	<div class="textwidget custom-html-widget"> 
+		    		<h4 class="online-step-numbers">1</h4>						
+		    		<div class="online-icon-container">
+		    			<i class="fa fa-laptop"></i>
+		    		</div>
+					<h4 class="online-cta-text">Shop by category, brand or strain</h4>
+				</div>
+			</div>
+        </div><!-- .coupon- -->
+
+        <div class="col-md coupon rounded online-cta">
+		    <div>
+		    	<div class="textwidget custom-html-widget">
+		    		<h4 class="online-step-numbers">2</h4>						
+		    		<div class="online-icon-container">
+		    			<i class="fa fa-cart-plus"></i>
+		    		</div>
+					<h4 class="online-cta-text">Checkout and select a pickup time</h4>
+				</div>
+			</div>
+        </div><!-- .coupon -->
+
+        <div class="col-md coupon rounded online-cta">
+		    <div>
+		    	<div class="textwidget custom-html-widget">
+		    		<h4 class="online-step-numbers">3</h4>						
+		    		<div class="online-icon-container">
+		    			<i class="fa fa-shopping-bag"></i>
+		    		</div>
+					<h4 class="online-cta-text">Pickup and pay at the store</h4>
+				</div>
+			</div>
+        </div><!-- .coupon -->
+
+        <div class="col-md coupon rounded joint online-cta">
+		    <div>
+		    	<div class="textwidget custom-html-widget">
+		    		<h4 class="online-step-numbers">4</h4>						
+		    		<div class="online-icon-container">
+		    			<i class="fa fa-joint"></i>
+		    		</div>
+					<h4 class="online-cta-text">OK, there are actually four steps ;) Spark it up!</h4>
+				</div>
+			</div>
+        </div><!-- .coupon -->
+
+    </div><!-- .row -->
+
+</div><!-- .container-fluid -->

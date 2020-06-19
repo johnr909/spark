916700ce0e8a3e8acf939be9c4a76c135a8627c2
john <johnjrose@gmail.com>
2020-06-19 15:03:57 -0600

    adding a couple of carousel files

---
+++ template-parts/content-testimonial-carousel-ex.php
@@ -0,0 +1,40 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+      <p>first slide</p>
+    </div>
+
+
+
+    <div class="carousel-item">
+      			    <blockquote>
+						<p class="testimonial-text">Only shop I go to. The people in there are super cool and know what they are talking about. The prices are UNBEATABLE. You cannot show me better pricing in Denver or in Colorado for that matter. Top quality all around, come here you won't regret it.
+
+							</p>
+							<p><cite title="Mitch H">Mitch H</cite></p>
+						</blockquote>
+
+    </div>
+
+
+    <div class="carousel-item">
+      			    <blockquote>
+						<p class="testimonial-text">Only shop I go to. The people in there are super cool and know what they are talking about. The prices are UNBEATABLE. You cannot show me better pricing in Denver or in Colorado for that matter. Top quality all around, come here you won't regret it.
+
+							</p>
+							<p><cite title="Mitch H">Mitch H</cite></p>
+						</blockquote>
+    </div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
---
+++ template-parts/content-testimonial-carousel.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Template part for the home page testimonials in carousel
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+<h3 class="lead text-center">Reviews</h3>
+
+<div id="carouselTestimonials" class="carousel slide carousel-fade" data-ride="carousel">
+
+  <div class="carousel-inner">
+
+    <div class="carousel-item active" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-cannabis"></i>&nbsp;&nbsp;
+						Only shop I go to. The people in there are super cool 
+						and know what they are talking about. The prices are UNBEATABLE. You cannot show me 
+						better pricing in Denver or in Colorado for that matter. Top quality all around, 
+						come here you won't regret it.</p>
+						<p><cite title="Mitch H">Mitch H</cite></p>
+				</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+			 <blockquote>
+					<p class="testimonial-text"><i class="fa fa-seedling"></i>&nbsp;&nbsp;The absolute best dispensary in Denver hands down. I live 40 minutes away from this location and it is still the only dispensary I will go to. Every member of the staff is incredibly friendly and knowledgeable and are always willing to help their customers. Other dispensaries in Denver could learn some things from how well this place is ran.
+					</p>
+					<p><cite title="Sean W">Sean W</cite></p>
+			</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-cannabis"></i>&nbsp;&nbsp;This dispensary is by far the best I've found since 
+						living in Denver. Vanilla kush is always a great pick for an indica. All the budtenders that I've shopped with have been very knowledgeable and patient.
+Oh, and if you have a med card spark has the absolute best sign over deals this side of the moon. Besides what the picture lists, you also get a $10 ounce upon sign over. 
+Thank you spark dispensary</p>
+						<p><cite title="Chris R">Chris R</cite></p>
+					</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-seedling"></i>&nbsp;&nbsp;Great dispensary. I went in for medical, and the whole experience was better than most places. It has the usual checks and security, but without it being quite so in your face. Once you get inside, it is a very nice, clean and professional looking setup. The people I met there are very friendly and helpful. The prices are very good, and should be even better on deal days. The selection is better for medical than most places I have been too. They have a good range of edibles, and some of the best quality and potency in their flower too. The only issue I had was parking. There are spots to park on the street, but not many you can park in legally. There are lots nearby, but some are posted. Definitely not the best parking situation I have seen at dispensaries, but the rest of the experience was great. For medical patients, this is the best I have been too. The only issue I had, which is very minor, is that you are supposed to get a free T-shirt with a $100 purchase. I did not realize that when I went in, so did not ask about it, but I was also not told about it at the time too. Very minor thing, but if you want a free shirt, be sure to ask for it.
+						</p>
+						<p><cite title="Johnnie D">Johnnie D</cite></p>
+			</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-cannabis"></i>&nbsp;&nbsp;Pre-order online for great medical flower and edibles great staff knowledgeable about products and the flower is top notch,also they love Veterans
+						</p>
+						<p><cite title="Darryl H">Darryl H</cite></p>
+					</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-seedling"></i>&nbsp;&nbsp;New Favorite place. Amazing customer service. Budtenders r knowledgeably about product's and most importantly prices and deals. Has amazing product. as an industry employee quality is #1 clean and well grown products. 100% Satisfactory.
+						</p>
+						<p><cite title="5280 Muzik">5280 Muzik</cite></p>
+					</blockquote>
+    </div><!-- .carousel-item -->
+
+    <div class="carousel-item" data-interval="3000">
+		    <blockquote>
+					<p class="testimonial-text"><i class="fa fa-cannabis"></i>&nbsp;&nbsp;I had a great experience there and they have a product called Ripple that is $5 cheaper than I can find anywhere else. Friendly staff as well. I actually go out of my way to go there when I am in town. I think they do a fine job. Thanks!
+						</p>
+						<p><cite title="Jenetts Jewells">Jenetts Jewells</cite></p>
+					</blockquote>
+    </div><!-- .carousel-item -->
+
+	</div><!-- .carousel-inner -->
+
+<!-- 	<a class="carousel-control-prev" href="#carouselTestimonials" role="button" data-slide="prev">
+	  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	  <span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselTestimonials" role="button" data-slide="next">
+	  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	  <span class="sr-only">Next</span>
+	</a>
+ -->
+
+</div><!-- #carousel -->

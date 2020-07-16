d51d3ec9a21d7920b86dd68979b939415555fb6f
john <johnjrose@gmail.com>
2020-07-16 15:27:05 -0600

    layout edits for home page

--- home.php
+++ home.php
@@ -31,19 +31,19 @@ get_header('spark'); ?>
 
     <div class="container">
         <div class="row home-cards">
-            <div id="card-home" class="col-sm-4 col-md card card-home">
+            <div id="card-home" class="col-sm-12 col-md-4 card card-home">
                 <?php
                     dealoFDay();
                 ?>
             </div><!-- .col -->
 
-            <div class="col-sm-4 col-md rewards-signup card card-home">
+            <div class="col-sm-12 col-md-4 rewards-signup card card-home">
 				<?php
 				    get_template_part( 'template-parts/content', 'rewards-signup' );
 				?>
             </div><!-- .col -->
 
-            <div class="col-sm-4 col-md online-cta-home card card-home">
+            <div class="col-sm-12 col-md-4 online-cta-home card card-home">
                 <h5 class="rewards-title">&nbsp;&nbsp;Product Spotlight</h5>
                 <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/Binske-gummies-r4.jpg'; ?>" class="card-img-top" alt="Binske gummies">
                 <p class="mt-3">Binske is an artisanal brand that prides itself on high-quality ingredients, consistency and style. Embracing a hands-on, homegrown culture, Binske sources the best raw ingredients from around the world to flavor their gummies and other edibles. Artists are employed to create unique pieces of art for each product package.</p>
@@ -53,13 +53,13 @@ get_header('spark'); ?>
 
     <div class="container">
         <div class="row home-cards">
-            <div class="col-sm-4 col-md card card-home">
+            <div class="col-sm-12 col-md-4 card card-home">
                 <?php
                     get_template_part( 'template-parts/content', 'testimonial-carousel' );
                 ?>
             </div><!-- .col -->
 
-            <div class="col-sm-4 col-md card card-home">
+            <div class="col-sm-12 col-md-4 card card-home">
                 <h5 class="rewards-title">Education Callout</h5>
                 <img src="
                 <?php echo get_template_directory_uri() . '/inc/assets/images/sativa-indica-r2.jpg'; ?>
@@ -69,7 +69,7 @@ get_header('spark'); ?>
                     and more</a>.</p>
             </div><!-- .col -->
 
-            <div class="col-sm-4 col-md pre-order-home card card-home">
+            <div class="col-sm-12 col-md-4 pre-order-home card card-home">
                 <h5 class="rewards-title">Pre-Order Online</h5>
                 <div style="width: 10rem;" class="mx-auto">
                     <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/hand.svg'; ?>" width="40%" height="40%" class="d-block mx-auto" alt="laptop">

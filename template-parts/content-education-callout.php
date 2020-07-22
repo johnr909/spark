9f0d982910d48412d2df4c8e489bdd3bdbca71b3
john <johnjrose@gmail.com>
2020-07-22 14:24:02 -0600

    re-did home page product spotlight and education callout blocks as widgets part 2

---
+++ template-parts/content-education-callout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for the home page education callout
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Education Callout") ) : ?>
+	<?php endif;?>
+
+
+
---
+++ template-parts/content-product-spotlight.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for the home page product spotlight
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Product Spotlight") ) : ?>
+	<?php endif;?>

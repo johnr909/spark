04030671cd600d87298bcd2da17c670e2921df02
john <johnjrose@gmail.com>
2020-08-10 16:01:30 -0600

    was  missing an alt tag on the hand.svg

--- home.php
+++ home.php
@@ -68,7 +68,7 @@ get_header('spark'); ?>
             <div class="col-sm-12 col-md-4 pre-order-home card card-home">
                 <h5 class="rewards-title text-center">Pre-Order Online</h5>
                 <div style="width: 10rem;" class="mx-auto">
-                    <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/hand.svg'; ?>" width="40%" height="40%" class="d-block mx-auto" alt="laptop">
+                    <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/hand.svg'; ?>" width="40%" height="40%" class="d-block mx-auto" alt="hand">
                 </div>
                 <p class="rewards-text green-text mt-2 text-center">It's soooo easy!</p>
                 <a href="/rec" class="d-block mx-auto mt-3 mb-3 btn-join btn btn-lg btn-primary">Pre-Order Rec</a>

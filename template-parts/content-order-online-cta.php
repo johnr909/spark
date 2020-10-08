3e6dc94a076cc877f2fda197c33d55c83cdc207a
john <johnjrose@gmail.com>
2020-10-08 15:53:11 -0600

    online CTA resizing

--- template-parts/content-order-online-cta.php
+++ template-parts/content-order-online-cta.php
@@ -6,7 +6,6 @@
  *
  * @package WP_Bootstrap_Starter
  */
-
 ?>
 
 <div class="container-fluid">
@@ -14,7 +13,7 @@
     <div class="row">
         <div class="col">
             <h3 class="lead">
-            	</i>&nbsp;&nbsp;Pre-Order Online: It's as easy as 1-2-3-4!</h3>
+            	</i>&nbsp;&nbsp;Pre-Order Online: It's as easy as 1-2-3...Enjoy!</h3>
         </div>
     </div>
     <!-- .row -->
@@ -23,33 +22,33 @@
 
        <div class="col-sm-5 col-md coupon rounded online-cta">
 		    		<h4 class="online-step-numbers dark-text">1</h4>
-					  <p class="online-cta-text mt-5 green-text text-center">Shop by category, brand or strain</p>
+					  <p class="online-cta-text green-text text-center">Shop by category, brand or strain</p>
 		    		<div class="online-icon-container step1">
-		    			<img src="../wp-content/themes/spark/inc/assets/images/hand2.svg" alt="hand" class="d-block img-hand" width="70%" height="70%">
+		    			<img src="../wp-content/themes/spark/inc/assets/images/hand2.svg" alt="hand" class="d-block img-hand" width="25%" height="25%">
 		    		</div>
         </div><!-- .coupon- -->
 
         <div class="col-sm-5 col-md coupon rounded online-cta">
 		    		<h4 class="online-step-numbers dark-text">2</h4>
-						<p class="online-cta-text mt-5 green-text text-center">Checkout and select a pickup time</p>
+						<p class="online-cta-text green-text text-center">Checkout and select a pickup time</p>
 		    		<div class="online-icon-container step2">
-		    			<img src="../wp-content/themes/spark/inc/assets/images/cart2.svg" alt="cart" class="d-block img-cart" width="90%" height="90%">
+		    			<img src="../wp-content/themes/spark/inc/assets/images/cart2.svg" alt="cart" class="d-block img-cart" width="33%" height="33%">
 		    		</div>
         </div><!-- .coupon -->
 
         <div class="col-sm-5 col-md coupon rounded online-cta">
 		    		<h4 class="online-step-numbers dark-text">3</h4>
-						<p class="online-cta-text mt-5 green-text text-center">Pickup and pay at the store</p>
+						<p class="online-cta-text green-text text-center">Pickup and pay at the store</p>
 		    		<div class="online-icon-containers step3">
-		    			<img src="../wp-content/themes/spark/inc/assets/images/bag2.svg" alt="bag" class="d-block img-bag" width="70%" height="70%">
+		    			<img src="../wp-content/themes/spark/inc/assets/images/bag2.svg" alt="bag" class="d-block img-bag" width="25%" height="25%">
 		    		</div>
         </div><!-- .coupon -->
 
         <div class="col-sm-5 col-md coupon rounded online-cta">
 		    		<h4 class="online-step-numbers dark-text">4</h4>
-					  <p class="online-cta-text mt-5 green-text text-center">Enjoy!</p>
+					  <p class="online-cta-text green-text text-center">Enjoy!</p>
 		    		<div class="online-icon-container step4">
-		    			<img src="../wp-content/themes/spark/inc/assets/images/emoji2.svg" alt="joint" class="d-block img-joint" width="90%" height="90%">
+		    			<img src="../wp-content/themes/spark/inc/assets/images/emoji2.svg" alt="joint" class="d-block img-joint" width="30%" height="30%">
 		    		</div>
         </div><!-- .coupon -->
 

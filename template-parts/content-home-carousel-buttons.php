8c1a1715404b3c4fb2c4b0e794847e244c1a8551
john <johnjrose@gmail.com>
2021-03-20 14:44:29 -0600

    got this sorta working

--- template-parts/content-home-carousel-buttons.php
+++ template-parts/content-home-carousel-buttons.php
@@ -1,7 +1,7 @@
 <div id="carousel-text" class="carousel-text">
     <h1>Spark Dispensary</h1>
-    <p class="lead">Denver’s premier, upscale medical and recreational marijuana dispensary</p>
-    <p class="cta">Save time, pre-order online!</p>
+    <p class="lead text-center white-text">Denver’s premier, upscale medical and recreational marijuana dispensary</p>
+    <p class="cta text-center green-text">Save time, pre-order online!</p>
     <a class="btn btn-primary btn-lg rec-button" href="/rec" role="button">Rec Menu</a>
     <a class="btn btn-secondary btn-lg med-button" href="/med" role="button">Med Menu</a>
 </div><!-- #carousel-text -->
--- template-parts/content-home-carousel.php
+++ template-parts/content-home-carousel.php
@@ -1,28 +1,33 @@
-<div id="carousel" class="carousel slide carousel-fade"" data-ride="carousel">
+<div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
 
     <div class="carousel-inner">
 
-        <div class="carousel-item active" data-interval="5000">
-            <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'leaf-closeup-1920w-350h.jpg'; ?>"
-                 class="d-block w-100" alt="marijuana leaf close-up">
-            <div class="carousel-caption d-none d-md-block">
-	            <?php
-	                get_template_part( 'template-parts/content', 'home-carousel-buttons' );
-	            ?>
+        <div class="carousel-item carousel-item1 active" data-interval="3000">
+            <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'westword-bg-vote-1920w-350h.jpg'; ?>"
+                 class="d-block mx-auto" alt="Westword vote background">
+            <div class="carousel-caption ww-vote-caption d-none d-md-block">
+            
+                <div class="ww-vote-bestof mx-auto white-bg">
+                    <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'westword-vote-logo.jpg'; ?>"
+                         width="50%" height="50%" class="d-block mx-auto" alt="Westword vote logo">
+                    <p class="best dark-text mt-0 mb-0 text-center text-uppercase font-weight-bold">Best Dispensary Bargains</p>
+                    <a href="https://bestofdenver2021.com/" target="_blank"
+                       class="ww-vote-btn mt-1 mb-1 text-center text-uppercase font-weight-bold white-text">vote for us</a>  
+                    <p class="ww-vote-text mt-0 mb-1 text-center text-uppercase font-weight-bold">the best of denver&reg; 2021 reader's choice award</p>
+                </div>
+	            
             </div><!-- .carousel-caption -->
         </div><!-- .carousel-item -->
 
-        <div class="carousel-item"data-interval="5000">
-            <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'taylor-1920w-350h.jpg'; ?>"
-                 class="d-block w-100" alt="marijuana plant close-up">
-            <div class="carousel-caption d-none d-md-block">
-	            <?php
-	                get_template_part( 'template-parts/content', 'home-carousel-buttons' );
-	            ?>
+        <div class="carousel-item carousel-item2" data-interval="3000">
+            <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'rebel-blasts-1920w-350h.jpg'; ?>"
+                 class="d-block w-100" alt="Rebel Blast edibles">
+            <div class="carousel-caption d-none d-md-block carousel-text">
+	            <p class="lead lead-dark">Rebel edibles BOGO 50% off all month</p>
             </div><!-- .carousel-caption -->
         </div><!-- .carousel-item -->
 
-        <div class="carousel-item" data-interval="5000">
+        <div class="carousel-item carousel-item3" data-interval="3000">
             <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'valdivia-1920w-350h.jpg'; ?>"
                  class="d-block w-100" alt="marijuana leaf close-up">
             <div class="carousel-caption d-none d-md-block">
@@ -32,7 +37,7 @@
             </div><!-- .carousel-caption -->
         </div><!-- .carousel-item -->
 
-        <div class="carousel-item bud-burst" data-interval="5000">
+        <div class="carousel-item carousel-item4 bud-burst " data-interval="3000">
             <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 'bud-burst-beauty-1920w-350h.jpg'; ?>"
                  class="d-block w-100" alt="marijuana leaf close-up">
             <div class="carousel-caption d-none d-md-block">

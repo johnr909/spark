a314111951c689c14e5a67d219f8f0987810cfc7
john <johnjrose@gmail.com>
2021-04-28 15:48:50 -0600

    i somehow put edited this file in master

---
+++ template-parts/content-home-rooster-ad.php
@@ -0,0 +1,5 @@
+<div id="carousel-text" class="carousel-text">
+    <h1>Spark Dispensary</h1>
+    <p class="lead text-center white-text">Rooster Ad...</p>
+    <p class="cta text-center green-text">rooster stuff...</p>
+</div><!-- #carousel-text -->

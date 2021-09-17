ceb4ba4654db775d9670ffec539255134e1e6322
John Rose <johnjrose@gmail.com>
2021-09-17 12:48:21 -0600

    simple fix to make rooster ad banner month text dynamic with PHP

--- template-parts/content-home-rooster-ad.php
+++ template-parts/content-home-rooster-ad.php
@@ -2,5 +2,5 @@
     <h1>Spark Dispensary</h1>
         <p class="lead text-center white-text rooster-text">Spark up your world!</p>
         <p class="cta text-center dark-text"><a class="rooster-link white-text" 
-href="/monthly-specials">Check out our August deals!</a></p>
+href="/monthly-specials">Check out our <?php echo date('F'); ?> deals!</a></p>
 </div><!-- #carousel-text -->

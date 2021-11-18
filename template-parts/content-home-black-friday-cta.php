fad12f4f5b3d67737d12680b5a8b0a5230a64d26
John Rose <johnr909@yahoo.com>
2021-11-18 15:00:23 -0700

    aded in BF banner to homepage rotator

---
+++ template-parts/content-home-black-friday-cta.php
@@ -0,0 +1,5 @@
+<div id="carousel-text" class="carousel-text carousel-rooster">
+    <h1>Spark Dispensary</h1>
+        <p class="lead text-center white-text rooster-text"><a class="rooster-link white-text" 
+href="/blackfriday">Spark it up on Black Friday!</a></p>
+</div><!-- #carousel-text -->
--- template-parts/content-home-carousel.php
+++ template-parts/content-home-carousel.php
@@ -4,6 +4,17 @@
 
         <div class="carousel-item active" data-interval="5000">
             <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 
+'black-friday-bg-banner-1920w-350h.webp'; ?>"
+                 class="d-block w-100" alt="our monthly specials">
+            <div class="carousel-caption rooster-caption d-md-block">
+                <?php
+                    get_template_part( 'template-parts/content', 'home-black-friday-cta' );
+                ?>
+            </div><!-- .carousel-caption -->
+        </div><!-- .carousel-item -->
+
+        <div class="carousel-item" data-interval="5000">
+            <img src="<?php echo get_template_directory_uri() . '/inc/assets/images/' . 
 'hiker-mtns-1920w-350h.jpg'; ?>"
                  class="d-block w-100" alt="our monthly specials">
             <div class="carousel-caption rooster-caption d-md-block">

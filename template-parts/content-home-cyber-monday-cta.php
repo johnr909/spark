61168c27d193d51b11f5e614a2e0e83c0e578d53
John Rose <johnr909@yahoo.com>
2021-11-29 11:15:03 -0700

    direct master edit-made error on Cyber Monday URL

--- template-parts/content-home-cyber-monday-cta.php
+++ template-parts/content-home-cyber-monday-cta.php
@@ -1,5 +1,5 @@
 <div id="carousel-text" class="carousel-text carousel-rooster">
     <h1>Spark Dispensary</h1>
         <p class="lead text-center white-text rooster-text"><a class="rooster-link white-text" 
-href="/blackfriday">Join Us for Cyber Monday!</a></p>
+href="/cybermonday">Join Us for Cyber Monday!</a></p>
 </div><!-- #carousel-text -->

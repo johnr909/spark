fa8f4c5191118b94fb348aa449583fc775c0b1e4
John Rose <johnr909@yahoo.com>
2022-05-28 22:01:03 -0600

    Just testing PHPCS WP on 'blank-page.php'.

--- blank-page.php
+++ blank-page.php
@@ -8,9 +8,9 @@ get_header();
     <section id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <?php
-                while ( have_posts() ) : the_post();
-                    get_template_part( 'template-parts/content', 'notitle' );
-                endwhile; // End of the loop.
+            while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/content', 'notitle' );
+            endwhile; // End of the loop.
             ?>
         </main><!-- #main -->
     </section><!-- #primary -->

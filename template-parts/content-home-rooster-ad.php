44268bbb16c8d31b04868896d015fa5b432cb290
John Rose <johnr909@yahoo.com>
2022-07-26 16:28:02 -0600

    Init commit to stub out the request to change the carousel and deals pages.

--- template-parts/content-home-rooster-ad.php
+++ template-parts/content-home-rooster-ad.php
@@ -1,13 +1,22 @@
-<div id="carousel-text" class="carousel-text carousel-rooster">
-    <h1>Spark Dispensary</h1>
-        <p class="lead text-center white-text rooster-text">
-            <?php _e('Spark up your world!', 'spark'); ?></p>
-        <p class="cta text-center dark-text">
-            <a class="rooster-link white-text" 
-                href="<?php echo esc_url('/monthly-specials', 'spark'); ?>">
-                <?php 
-                    _e('Check out our ', 'spark'); echo date('F'); _e(' deals!' ,'spark'); 
-                ?>
-          </a>
-        </p>
+<div id="carousel-text" class="carousel-text">
+    <h1><?php _e( 'Spark Dispensary', 'spark'); ?></h1>
+    <p class="lead text-center white-text rooster-text">
+            <?php _e('Spark up your world!', 'spark'); ?>
+    </p>
+    <p class="cta text-center dark-text">
+        <a class="rooster-link white-text" 
+            href="<?php echo esc_url('/monthly-specials', 'spark'); ?>">
+            <?php 
+                _e('Check out our ', 'spark'); echo date('F'); _e(' deals!' ,'spark'); 
+            ?>
+      </a>
+    </p>
+    <p class="cta text-center green-text">
+        <?php _e( 'Save time, pre-order online!', 'spark' ); ?></p>
+    <a class="btn btn-primary btn-lg rec-button" 
+       href="<?php echo esc_url( '/rec', 'spark' ); ?>" role="button">
+       <?php _e( 'Rec Menu', 'spark' ); ?></a>
+    <a class="btn btn-secondary btn-lg med-button" 
+       href="<?php echo esc_url( '/med', 'spark' ); ?>" role="button">
+       <?php _e( 'Med Menu', 'spark' ); ?></a>
 </div><!-- #carousel-text -->
